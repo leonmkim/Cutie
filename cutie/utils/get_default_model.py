e1c30313d81761525158470932e360ca40138cf2
Leon Kim <leonmkim@seas.upenn.edu>
2024-04-03 15:43:51 -0400

    changed get default model

--- cutie/utils/get_default_model.py
+++ cutie/utils/get_default_model.py
@@ -11,17 +11,17 @@ from scripts.download_models import download_models_if_needed
 
 
 def get_default_model() -> CUTIE:
-    initialize(version_base='1.3.2', config_path="../config", job_name="eval_config")
-    cfg = compose(config_name="eval_config")
+    with initialize(version_base='1.3.2', config_path="../config", job_name="eval_config"):
+        cfg = compose(config_name="eval_config")
 
-    download_models_if_needed()
-    with open_dict(cfg):
-        cfg['weights'] = './weights/cutie-base-mega.pth'
-    get_dataset_cfg(cfg)
+        download_models_if_needed()
+        with open_dict(cfg):
+            cfg['weights'] = './weights/cutie-base-mega.pth'
+        get_dataset_cfg(cfg)
 
-    # Load the network weights
-    cutie = CUTIE(cfg).cuda().eval()
-    model_weights = torch.load(cfg.weights)
-    cutie.load_weights(model_weights)
+        # Load the network weights
+        cutie = CUTIE(cfg).cuda().eval()
+        model_weights = torch.load(cfg.weights)
+        cutie.load_weights(model_weights)
 
     return cutie
