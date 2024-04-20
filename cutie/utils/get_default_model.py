d2eb8115e145b235c49911b5e94783f5a1aac86b
leon <leonmkim@seas.upenn.edu>
2024-04-19 21:17:03 -0400

    get around global hydra init

--- cutie/utils/get_default_model.py
+++ cutie/utils/get_default_model.py
@@ -9,14 +9,24 @@ from cutie.model.cutie import CUTIE
 from cutie.inference.utils.args_utils import get_dataset_cfg
 from scripts.download_models import download_models_if_needed
 
+import os, sys, omegaconf
+import hydra
 
 def get_default_model() -> CUTIE:
+    # TODO this might be a hack with unintended side effects...
+    hydra.core.global_hydra.GlobalHydra.instance().clear()
     with initialize(version_base='1.3.2', config_path="../config", job_name="eval_config"):
         cfg = compose(config_name="eval_config")
+    
+        # # get current file path
+        # current_path = os.path.dirname(os.path.realpath(__file__))
+        # # get the relative path to the config file
+        # config_file_path = os.path.join(current_path, "../config/eval_config.yaml")
+        # cfg = omegaconf.OmegaConf.load(config_file_path)
 
-        download_models_if_needed()
+        weights_root_dirpath=download_models_if_needed()
         with open_dict(cfg):
-            cfg['weights'] = './weights/cutie-base-mega.pth'
+            cfg['weights'] = os.path.join(weights_root_dirpath, 'cutie-base-mega.pth')
         get_dataset_cfg(cfg)
 
         # Load the network weights
@@ -24,4 +34,4 @@ def get_default_model() -> CUTIE:
         model_weights = torch.load(cfg.weights)
         cutie.load_weights(model_weights)
 
-    return cutie
+        return cutie
--- scripts/download_models.py
+++ scripts/download_models.py
@@ -9,24 +9,32 @@ _links = [
     ('https://github.com/hkchengrex/Cutie/releases/download/v1.0/cutie-base-mega.pth', 'a6071de6136982e396851903ab4c083a'),
 ]
 
+
 def download_models_if_needed():
-    os.makedirs('weights', exist_ok=True)
+    # get current file path
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    # get a relative path to weights
+    weights_path = os.path.join(current_path, '../weights')
+
+    os.makedirs(weights_path, exist_ok=True)
     for link, md5 in _links:
         # download file if not exists with a progressbar
         filename = link.split('/')[-1]
-        if not os.path.exists(os.path.join('weights', filename)) or hashlib.md5(open(os.path.join('weights', filename), 'rb').read()).hexdigest() != md5:
+        if not os.path.exists(os.path.join(weights_path, filename)) or hashlib.md5(open(os.path.join(weights_path, filename), 'rb').read()).hexdigest() != md5:
             print(f'Downloading {filename}...')
             r = requests.get(link, stream=True)
             total_size = int(r.headers.get('content-length', 0))
             block_size = 1024
             t = tqdm(total=total_size, unit='iB', unit_scale=True)
-            with open(os.path.join('weights', filename), 'wb') as f:
+            with open(os.path.join(weights_path, filename), 'wb') as f:
                 for data in r.iter_content(block_size):
                     t.update(len(data))
                     f.write(data)
             t.close()
             if total_size != 0 and t.n != total_size:
                 raise RuntimeError('Error while downloading %s' % filename)
+    
+    return weights_path
             
 
 if __name__ == '__main__':
